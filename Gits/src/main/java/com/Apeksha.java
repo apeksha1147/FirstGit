8a92c45e29b28b0e54473b9b81b5dbdd9a4c7b81
grvsen <grvsen45@gmail.com>
2020-04-01 16:09:57 +0530

    nc

--- Gits/src/main/java/com/Apeksha.java
+++ Gits/src/main/java/com/Apeksha.java
@@ -2,6 +2,9 @@ package com;
 
 public class Apeksha {
 public static void main(String[] args) {
-	System.out.println("hello");
+	for(int i=0;i<10;i++)
+		{System.out.println("hello");
+		
+		}
 }
 }
